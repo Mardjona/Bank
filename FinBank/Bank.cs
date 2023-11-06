c1bacf6e4e2584f151810e5e7aa74588188928ef
Mardjona <114858484+Mardjona@users.noreply.github.com>
2023-11-06 17:05:36 +0300

    Bank.cs
    Обновленный

--- FinBank/Bank.cs
+++ FinBank/Bank.cs
@@ -1,18 +1,19 @@
-﻿class Bank
+class Bank
 {
     private int nom;
     private string name;
     private float sum;
     public List<Bank> list;
-    public Bank(string n) { nom = 1; name = n; sum = 10000f; }
-    public Bank(string n, float s) { nom = 2; name = n; sum = s; }
-    public Bank(int id, string n, float s) { nom = id; name = n; sum = s; }
+    public Bank(string n) { nom = 1; name = n; sum = 1000f; } // конструктор 1
+    public Bank(string n, float s) { nom = 2; name = n; sum = s; } // конструктор 2
+    public Bank(int id, string n, float s) { nom = id; name = n; sum = s; } // конструктор 3
 
     //открытие счета
     public void Out()
     {
-        Console.WriteLine($"Номер вашего счета: {nom}\tВаше имя: {name}\t Cумма на счете: {sum}");
-    }
+         Console.WriteLine($"Номер вашего счета: {nom}\tВаше имя: {name}\t Cумма на счете: {sum}");
+     }
+
     // метод чтоб положить деньги на счет
     public void dob()
     {
@@ -21,12 +22,13 @@
         sum += a;
         Console.WriteLine($"Деньги успешно внесены, на балансе {sum} рублей ");
     }
+
     // метод чтоб снять деньги со счета
     public void umen()
     {
-       Console.Write("Сколько рублей хотите снять?");
+        Console.Write("Сколько рублей хотите снять?");
         int a = Convert.ToInt32(Console.ReadLine());
-        if (a <= sum)
+        if (a <= sum) 
         {
             sum -= a;
             Console.WriteLine($"Снято {a} рублей.На балансе {sum} рублей");
@@ -53,10 +55,13 @@
 
         Console.WriteLine("Сколько хотите перевести?");
         int r = Convert.ToInt32(Console.ReadLine());
-        sum -= r;
-        Searh(i, list).sum += r;
+        sum -= r;                              // вычитается сумма с заданного счета
+       
+        Searh(i, list).sum += r;               // прибавляется сумма в выбранный счет
+       
     }
 
+    // перебор счетов
     private Bank Searh(int nom, List<Bank> bank)
     {
 
@@ -64,18 +69,24 @@
         {
             if (nom == b.nom)
             {
+                Console.WriteLine($"Перевод успешно выполнен! На вешем счету {sum}");
                 return b;
+                
             }
-
+            
         }
-        Console.WriteLine("Счет не найден");
+
+        Console.WriteLine("Счет не найден.Повторите действие");
+        Print();
         return null;
+       
     }
+
+    // метод для возврата методов, опираясь на выбор клиента
     public void Print()
     {
-        
         {
-            
+            Out();
             Console.WriteLine("Выберите операцию для выполнения:\n1 - Внести деньги\t2 - Снять деньги\t3 - Снять всю сумму\t4 - перевести деньги на другой счет\t5 - Выход");
             int a = Convert.ToInt32(Console.ReadLine());
 
@@ -102,9 +113,9 @@
             else
             {
                 Console.WriteLine("Ошибка выбора");
+                Print();
             }
-
-            Console.WriteLine("-----------------------------Следующий---------------------");
+             Console.WriteLine("-----------------------------Следующий---------------------");
         }
     }
 }
