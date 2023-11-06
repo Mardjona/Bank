67304a114a04e9e38f9b5ece7e8683710d43f18a
Mardjona <114858484+Mardjona@users.noreply.github.com>
2023-11-06 18:52:23 +0300

    Program.cs

--- FinBank/Program.cs
+++ FinBank/Program.cs
@@ -1,32 +1,17 @@
 class Program
 {
-     static void Main(string[] args)
+    
+    static void Main(string[] args)
     {
+        var acc1 = new Bank(1, "Fel", 1000f);
+        var acc2 = new Bank(2, "Fibb", 500f);
+        var acc3 = new Bank(3, "Monica", 100f);
+        List<Bank> banks = new List<Bank>() { acc1, acc2, acc3 };
         while (true)
         {
-            
-          
-            var acc1 = new Bank("Fel");
-            var acc2 = new Bank("Fibb", 500f);
-            var acc3 = new Bank(3, "Monica", 100f);
-            acc1.list = new List<Bank>();
-
-            acc1.list.Add(acc2);
-            acc1.list.Add(acc3);
-           
-            acc1.Print();
-
-            acc2.list = new List<Bank>();
-            acc2.list.Add(acc1);
-            acc2.list.Add(acc3);
-           
-            acc2.Print();
-
-            acc3.list = new List<Bank>();
-            acc3.list.Add(acc1);
-            acc3.list.Add(acc2);
-           
-            acc3.Print();
+            acc1.Print(banks);
+            acc2.Print(banks);
+            acc3.Print(banks);
         }
     }
 }
