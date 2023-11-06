01118e56e92b6574f4b68a06b51458aac7f067ee
Mardjona <114858484+Mardjona@users.noreply.github.com>
2023-11-06 03:07:30 +0300

    Program.cs

--- FinBank/Program.cs
+++ FinBank/Program.cs
@@ -1,16 +1,28 @@
-﻿class Program
+class Program
 {
     static void Main(string[] args)
     {
 
         var acc1 = new Bank("Fel");
-        var acc2 = new Bank("Fibb", 5000f);
-        var acc3 = new Bank(3, "Monica", 10000f);
+        var acc2 = new Bank("Fibb", 500f);
+        var acc3 = new Bank(3, "Monica", 100f);
         acc1.list = new List<Bank>();
         acc1.list.Add(acc1);
         acc1.list.Add(acc2);
         acc1.list.Add(acc3);
+        acc1.Out();
         acc1.Print();
+        
+       acc2.list = new List<Bank>();
+        acc2.list.Add(acc2);
+        acc2.list.Add(acc3);
         acc2.Out();
+        acc2.Print();
+
+        acc3.list = new List<Bank>();
+         acc3.list.Add(acc1);
+         acc3.list.Add(acc2);
+        acc3.Out();
+        acc3.Print();
     }
 }
