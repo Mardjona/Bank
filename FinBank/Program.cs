6808a8dee0a063c13cd1dbca70a0dd1db474b443
Mardjona <114858484+Mardjona@users.noreply.github.com>
2023-11-10 01:22:28 +0300

    Update Program.cs

--- FinBank/Program.cs
+++ FinBank/Program.cs
@@ -1,17 +1,44 @@
 class Program
 {
-    
+   
+
     static void Main(string[] args)
     {
-        var acc1 = new Bank(1, "Fel", 1000f);
-        var acc2 = new Bank(2, "Fibb", 500f);
-        var acc3 = new Bank(3, "Monica", 100f);
+
+        var acc1 = new Bank();
+        var acc2 = new Bank();
+        var acc3 = new Bank();
         List<Bank> banks = new List<Bank>() { acc1, acc2, acc3 };
+
+        acc1.Info();
+        acc2.Info();
+        acc3.Info();
+
         while (true)
         {
-            acc1.Print(banks);
-            acc2.Print(banks);
-            acc3.Print(banks);
+           
+            Console.WriteLine("\nВведите на какой счет хотите перейти");
+            int i = Convert.ToInt32(Console.ReadLine());
+            if(i ==acc1.Nomer )
+            {
+                acc1.Print(banks);
+            }
+            else if (i == acc2.Nomer)
+            {
+                acc2.Print(banks);
+            }
+            else if (i == acc3.Nomer)
+            {
+                acc3.Print(banks);
+            }
+
+
+           
+            
+            
+            
+           
         }
     }
 }
+
